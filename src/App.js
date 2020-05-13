1412b6f586633d2db94178d152a9a8379e31091b
Sama Mushtaq <sama.mushtaq@rgare.com>
2020-05-13 15:33:46 -0500

    AddBtn and AddLogModal Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -15,7 +17,8 @@ function App() {
     <div className='App'>
       <SearchBar />
       <div className='container'>
-        {" "}
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </div>
---
+++ src/components/layout/AddBtn.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const AddBtn = () => {
+  return (
+    <div className='fixed-action-btn'>
+      <a
+        href='#add-log-modal'
+        className='btn-floating btn-large large blue darken-2 modal-trigger'
+      >
+        <i className='fas fa-plus-circle'></i>
+      </a>
+      <ul>
+        <li>
+          <a
+            href='#tech-list-modal'
+            className='btn-floating green modal-trigger'
+          >
+            <i className='fas fa-user'></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href='#tech-modal tooltipped'
+            className='btn-floating purple modal-trigger'
+          >
+            <i className='fas fa-user-plus'></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AddBtn;
