77b4d57ae46dc4d48a62e7782e9c30d01c4a6127
Sama Mushtaq <sama.mushtaq@rgare.com>
2020-05-19 15:38:46 -0500

    Connecting Redux to a component

---
+++ src/actions/logActions.js
@@ -0,0 +1,51 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SEARCH_LOGS,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+} from "./types";
+
+// export const getLogs = () => {
+//   return async (dispatch) => {
+
+//     setLoading();
+
+//     const res = await fetch("/logs");
+//     const data = await res.json();
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+//set Loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
+
+//get logs from server
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
